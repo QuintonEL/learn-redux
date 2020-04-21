8009c9d05e29e7691dc8e41b5967133c22b5a931
QuintonEL <quintonlaborde@gmail.com>
2020-04-21 13:21:42 -0600

    all state are working and combined

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore } from 'redux';
+import allReducers from './reducers';
+
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
