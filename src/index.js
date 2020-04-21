e08536f1c909422b0297d5ffe92c2acb6f56d5d7
QuintonEL <quintonlaborde@gmail.com>
2020-04-21 12:51:35 -0600

    began refactoring

--- src/index.js
+++ src/index.js
@@ -2,45 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { createStore } from 'redux';
-
-
-
-//STORE -> GLOABALIZED STATE
-
-//ACTION -> INCREMENT
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-
-//REDUCER -> DECIDES WHAT TO DO WITH THE ACTION
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-  }
-}
-
-let store = createStore(counter);
-
-//DISPLAY IN THE CONSOLE
-store.subscribe(() => console.log(store.getState()));
-
-//DISPATCH -> SET THAT ACTION
-store.dispatch(increment());
-store.dispatch(decrement());
-
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
